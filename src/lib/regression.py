5b1f3a6270fe96c2fa8bbd1d8c9f75417113127a
Mathias <hmvege@ulrik.uio.no>
2018-10-01 22:21:21 +0200

    Added linear regression. Need to add a test module as well, perhaps split the regression analysis into different folders.

--- src/lib/regression.py
+++ src/lib/regression.py
@@ -1,33 +1,38 @@
 import numpy as np
 import scipy.linalg
 
+# help(super)
 class __reg_backend:
-    possible_backends = ["numpy", "scipy"]
-    linalg_backend = "numpy"
+    __fit_performed = False
+    __possible_backends = ["numpy", "scipy"]
 
-    def _set_backend(self, linalg_backend):
+    def __init__(self, linalg_backend):
         """Sets up the linalg backend."""
-        assert linalg_backend in possible_backends, \
+        assert linalg_backend in self.__possible_backends, \
             "{} backend not recognized".format(linalg_backend)
         self.linalg_backend = linalg_backend
 
-    def _get_inv(self, M):
+    def __get_inv(self, M):
         if self.linalg_backend == "numpy":
             return np.linalg.inv(M)
         elif self.linalg_backend == "scipy":
             return scipy.linalg.inv(M)
 
-    def _get_sum(self, V):
+    def __get_sum(self, V):
         if self.linalg_backend == "numpy":
             return np.sum(V)
         elif self.linalg_backend == "scipy":
             return scipy.sum(V)
 
-    def _get_diag(self, M):
+    def __get_diag(self, M):
         if self.linalg_backend == "numpy":
             return np.diag(V)
         elif self.linalg_backend == "scipy":
             return scipy.diag(V)
+
+    def __check_if_fitted(self):
+        """Small check if fit has been performed."""
+        assert self.__fit_performed, "Fit not performed"
 
 
 class LinearRegression(__reg_backend):
@@ -38,13 +43,9 @@ class LinearRegression(__reg_backend):
     """
 
     def __init__(self, linalg_backend="numpy"):
+        super().__init__(linalg_backend)
         self.X_train = None
         self.y_train = None
-
-        # Sets up the linalg backend
-        assert linalg_backend in possible_backends, \
-            "{} backend not recognized".format(linalg_backend)
-        self.linalg_backend = linalg_backend
 
     def fit(self, X_train, y_train):
         """
@@ -63,7 +64,7 @@ class LinearRegression(__reg_backend):
         self.XTX = self.X_train.T @ self.X_train
 
         # (X^T * X)^{-1}
-        self.XTX_inv = self._get_inv(self.XTX)
+        self.XTX_inv = self.__get_inv(self.XTX)
 
         # Beta fit values: beta = (X^T * X)^{-1} @ X^T @ y
         self.coef = self.XTX_inv @ self.X_train.T @ self.y_train
@@ -75,15 +76,24 @@ class LinearRegression(__reg_backend):
         self.eps = self.y_train - self.y_approx
 
         # Variance of y approximate values. sigma^2
-        self.y_variance = self._get_sum(eps**2) / float(self.N)
+        self.y_variance = self.__get_sum(eps**2) / float(self.N)
 
         # Beta fit covariance/variance. (X^T * X)^{-1} * sigma^2
         self.coef_cov = self.XTX_inv * self.y_variance
-        self.coef_var = self._get_diag(self.coef_cov)
+        self.coef_var = self.__get_diag(self.coef_cov)
 
-        raise NotImplementedError
+        self.__fit_performed = True
 
     def predict(self, X_test):
+        """Performs a prediction for given beta coefs.
+
+        Args:
+            X_test (ndarray): 
+
+        Returns:
+            y (ndarray):
+        """
+        self.__check_if_fitted()
         return X_test @ self.coef
 
 
@@ -92,10 +102,13 @@ class RidgeRegression(__reg_backend):
     An implementation of ridge regression.
     """
 
-    def __init__(self, X_train, y_train):
-        raise NotImplementedError
+    def __init__(self):
+        super().__init__(linalg_backend)
+        self.X_train = None
+        self.y_train = None
 
-    def fit():
+    def fit(self, X_train, X_train):
+        self.__fit_performed = True
         raise NotImplementedError
 
     def predict(self, X_test):
@@ -107,10 +120,13 @@ class LassoRegression(__reg_backend):
     An implementation of lasso regression.
     """
 
-    def __init__(self, X_train, y_train):
+    def __init__(self):
+        super().__init__(linalg_backend)
+        self.X_train = None
+        self.y_train = None
         raise NotImplementedError
 
-    def fit():
+    def fit(self, X_train, X_train):
         raise NotImplementedError
 
     def predict(self, X_test):
@@ -118,4 +134,5 @@ class LassoRegression(__reg_backend):
 
 
 if __name__ == '__main__':
+    Test = LinearRegression()
     exit("regression.py not intended as a standalone module.")
